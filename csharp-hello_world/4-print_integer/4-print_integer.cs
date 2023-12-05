1ab17203fd6eae56a142cb7dba8b02f2803edbad
Franco Correa <francorr240@gmail.com>
2023-12-05 05:51:13 -0300

    Fixed syntax

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -5,6 +5,6 @@ class Program
 	static void Main(string[] args)
 	{
 		int number = 972;
-		console.WriteLine("{0} Mission Street", number);
+		Console.WriteLine("{0} Mission Street", number);
 	}
 }
