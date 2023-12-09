64e836bc14bc144f711ed81b364d2a81c83db73e
Franco Correa <francorr240@gmail.com>
2023-12-09 18:22:29 -0300

    Task 6 complete

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int num = 0; num <= 9; num++)
+        {
+            for (int num1 = num + 1; num1 <= 9; num1++)
+            {
+                Console.Write("{0}{1}", num, num1);
+                if (!(num == 8 && num1 == 9))
+                {
+                    Console.Write(", ");
+                }
+                else
+                {
+                    Console.Write("\n");
+                }
+            }
+        }
+    }
+}
