be05f11f5cf5176ef21cfd2cc4d2383d48d5e755
Franco Correa <francorr240@gmail.com>
2023-12-09 19:32:29 -0300

    Task 11 complete

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        int space = 0;
+        if (length > 0)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int x = 0; x <= space; x++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("\\");
+                space++;
+            }
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+    }
+}
