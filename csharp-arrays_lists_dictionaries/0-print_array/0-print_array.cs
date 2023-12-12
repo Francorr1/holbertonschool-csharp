17a16eefb6350ebc126cbaec88863a0114cb4811
Franco Correa <francorr240@gmail.com>
2023-12-12 14:04:19 -0300

    Task 0 complete

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size == 0)
+        {
+            Console.WriteLine();
+            int[] newArray = new int[] {};
+            return newArray;
+        }
+        else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else
+        {
+            int[] newArray = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = i;
+                Console.Write("{0} ", i);
+                if (i == size - 1)
+                {
+                    Console.WriteLine();
+                }
+            }
+            return newArray;
+        }
+    }
+}
