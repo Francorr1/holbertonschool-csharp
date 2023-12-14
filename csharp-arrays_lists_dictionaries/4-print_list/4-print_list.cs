89b3510d70c0bff019b11504d4e8da3d126239cc
Franco Correa <francorr240@gmail.com>
2023-12-14 17:59:49 -0300

    Task 4 complete

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else
+        {
+            List<int> newList = new List<int>();
+            for (int i = 0; i < size; i++)
+            {
+                newList.Add(i);
+                Console.Write("{0}", i);
+                if (i != size)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+            return newList;
+        }
+    }
+}
