af231599225396bba5db459e23f8deaa89cf603a
Franco Correa <francorr240@gmail.com>
2023-12-14 19:10:26 -0300

    Task 8 complete

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int keys = 0;
+        foreach (var key in myDict.Keys)
+        {
+            keys++;
+        }
+        return keys;
+    }
+}
