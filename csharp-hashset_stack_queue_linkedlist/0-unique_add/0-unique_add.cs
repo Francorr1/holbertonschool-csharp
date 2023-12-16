30adb4b27e0160619a1f66678d99b4d8c90b0edf
Franco Correa <francorr240@gmail.com>
2023-12-16 16:34:11 -0300

    Task 0 complete

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        var singlesList = myList.Distinct().ToList();
+        int res = 0;
+        for (int i = 0; i < singlesList.Count; i++)
+        {
+            res += singlesList[i];
+        }
+        return res;
+    }
+}
