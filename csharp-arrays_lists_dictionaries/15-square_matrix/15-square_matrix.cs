2485e8c968a5b4acf296832f290c9f5e426d89a1
Franco Correa <francorr240@gmail.com>
2023-12-16 16:01:51 -0300

    Task 15 complete

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int x = myMatrix.GetLength(0);
+        int y = myMatrix.GetLength(1);
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                myMatrix[i, j] = (int)Math.Pow(myMatrix[i, j], 2);
+            }
+            return myMatrix;
+        }
+    }
+}
