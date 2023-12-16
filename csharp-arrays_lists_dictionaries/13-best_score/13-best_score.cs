46ad1db79bb1fcbb7c8d8d954aeb4d9ddc031660
Franco Correa <francorr240@gmail.com>
2023-12-16 03:20:12 -0300

    Task 13 complete

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        try
+        {
+            var maxValue = myList.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+            return maxValue;
+        }
+        catch (System.Exception)
+        {
+            return "None";
+            throw;
+        }
+    }
+}
