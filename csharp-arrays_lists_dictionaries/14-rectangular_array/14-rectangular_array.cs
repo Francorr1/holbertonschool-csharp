54b2141c2b9bc82c049e72a0abf79a161afb6d43
Franco Correa <francorr240@gmail.com>
2023-12-16 03:41:41 -0300

    Task 14 complete

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] newMatrix = new int[5, 5];
+            newMatrix[2, 2] = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("{0}", newMatrix[i, j]);
+                    if (j == 4)
+                    {
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+        }
+    }
+}
