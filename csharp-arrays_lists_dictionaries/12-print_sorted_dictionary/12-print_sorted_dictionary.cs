d9b88b9f38a52d5160ef54f2343b381d6c8cfe3b
Franco Correa <francorr240@gmail.com>
2023-12-16 03:06:11 -0300

    Task 12 complete

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var listcast = myDict.Keys.ToList();
+        listcast.Sort();
+        foreach (var key in listcast)
+        {
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+        }
+    }
+}
