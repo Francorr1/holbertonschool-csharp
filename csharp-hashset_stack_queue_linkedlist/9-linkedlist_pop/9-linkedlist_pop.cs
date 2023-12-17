fff8bb49a0855b2ce1614f067f5174fdc50d1c35
Franco Correa <francorr240@gmail.com>
2023-12-17 19:07:44 -0300

    Task 11 complete

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        try
+        {
+            int FirstValue = myLList.First.Value;
+            myLList.RemoveFirst();
+            return FirstValue;
+        }
+        catch (System.Exception)
+        {
+            return 0;
+            throw;
+        }
+    }
+}
