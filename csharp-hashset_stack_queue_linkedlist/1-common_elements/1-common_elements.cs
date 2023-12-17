6053e3a6ccf4864e7203ca7a478cb777bad176dc
Franco Correa <francorr240@gmail.com>
2023-12-17 14:13:47 -0300

    Task 1 complete

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> hash1 = new HashSet<int>(list1);
+        HashSet<int> hash2 = new HashSet<int>(list2);
+        hash1.IntersectWith(hash2);
+        List<int> common = new List<int>();
+        foreach (var item in hash1)
+        {
+            common.Add(item);
+        }
+        return common;
+    }
+}
