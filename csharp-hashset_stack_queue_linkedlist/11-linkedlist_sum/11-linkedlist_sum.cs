82ee0fa82240fcd20d4b8a49c1da73656ceea1fa
Franco Correa <francorr240@gmail.com>
2023-12-17 19:31:21 -0300

    Task 11  complete

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int res = 0;
+        foreach (var item in myLList)
+        {
+            res += item;
+        }
+        return res;
+    }
+}
