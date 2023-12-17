f99fbe6aa5c3708a4951669c5468fba4f0a2a512
Franco Correa <francorr240@gmail.com>
2023-12-17 18:34:09 -0300

    Task 5 complete

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> LList = new LinkedList<int>();
+        if (size < 0)
+        {
+            return LList;
+        }
+        else
+        {
+            for (int i = 0; i < size; i++)
+            {
+                LList.AddLast(i);
+                Console.WriteLine("{0}", i);
+            }
+            return LList;
+        }
+    }
+}
