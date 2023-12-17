114d5b26fbfe37cfc0a08bd35f41433afb537939
Franco Correa <francorr240@gmail.com>
2023-12-17 15:08:51 -0300

    Task 2 complete

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> newList1 = list1.FindAll(item => !list2.Contains(item));
+        List<int> newList2 = list2.FindAll(item => !list1.Contains(item));
+        newList1.AddRange(newList2);
+        newList1.Sort();
+        return newList1;
+    }
+}
