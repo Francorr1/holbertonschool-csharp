4c8e9e1987286bf75f8c3665bd00e17e8ee487de
Franco Correa <francorr240@gmail.com>
2023-12-17 19:16:44 -0300

    Task 10 complete

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int cont = 0;
+        foreach (var item in myLList)
+        {
+            if (cont == n)
+            {
+                return item;
+            }
+            cont++;
+        }
+        return 0;
+    }
+}
