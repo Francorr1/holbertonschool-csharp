84a6287a266a2cee8d471e29c4ba0b4f04926446
Franco Correa <francorr240@gmail.com>
2023-12-17 22:47:12 -0300

    Task 12 complete

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current = myLList.First;
+        if (current.Value > n)
+        {
+            myLList.AddFirst(n);
+            current = myLList.First;
+            return current;
+        }
+        while (current != null)
+        {
+            if (n < current.Value)
+            {
+                LinkedListNode<int> newnode = myLList.AddBefore(current, n);
+                return newnode;
+            }
+            current = current.Next;
+        }
+        current = myLList.AddLast(n);
+        return current;
+    }
+}
