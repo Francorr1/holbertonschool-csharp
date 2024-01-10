4a9599517d78b96ec964d4fcae1e64d61338d9b5
Franco Correa <francorr240@gmail.com>
2024-01-10 01:47:01 -0300

    Fixed documentation

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -7,7 +7,13 @@ namespace Enemies
     /// </summary>
     public class Zombie
     {
+        /// <summary>
+        /// Public int field that stores the health of the enemy
+        /// </summary>
         public int health;
+        /// <summary>
+        /// Public constructor that sets the health to 0
+        /// </summary>
         public Zombie()
         {
             health = 0;
