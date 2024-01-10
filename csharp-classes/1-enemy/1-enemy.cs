d7921c2bd836342b0de94868c21d0576ead1ad24
Franco Correa <francorr240@gmail.com>
2024-01-10 01:44:06 -0300

    Task 1 complete

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Class defining a Zombie and setting its health to 0
+    /// </summary>
+    public class Zombie
+    {
+        public int health;
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
