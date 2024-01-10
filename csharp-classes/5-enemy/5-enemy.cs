bf8cdedf8a125bea44844fdc28cd859c9a3dd90e
Franco Correa <francorr240@gmail.com>
2024-01-10 02:19:45 -0300

    Fixed output

--- csharp-classes/5-enemy/5-enemy.cs
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -62,7 +62,7 @@ namespace Enemies
         /// <returns></returns>
         public override string ToString()
         {
-            return "Zombie name: " + name + " / Total Health: " + health;
+            return "Zombie Name: " + name + " / Total Health: " + health;
         }
     }
 }
