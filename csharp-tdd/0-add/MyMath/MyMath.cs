5bc0961e2885eeb0866c90db691045b2b4dc75d7
Franco Correa <francorr240@gmail.com>
2024-01-14 11:03:36 -0300

    Testing a XML documentation fix

--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -1,7 +1,9 @@
 ﻿namespace MyMath;
 
+/// <summary>Class Operations</summary>
 public class Operations
 {
+    /// <summary>Adds two numbers</summary>
     public static int Add(int a, int b)
     {
         return a + b;
