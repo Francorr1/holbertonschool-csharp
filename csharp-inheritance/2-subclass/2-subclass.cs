0107908b0738eeda68d5cd8e82b111ae9ff5511b
Franco Correa <francorr240@gmail.com>
2024-01-16 07:15:22 -0300

    Task 2 complete

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class Obj
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Returns true only if the derived type is a subclass of baseType, otherwise false
+    /// </summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+        {
+            if (derivedType != baseType)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
