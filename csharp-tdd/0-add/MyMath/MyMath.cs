c992786eb713f5e306755b9e8c5fe7ab535a42ac
Franco Correa <francorr240@gmail.com>
2024-01-16 06:26:16 -0300

    10th times the charm

---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyMath;
+
+/// <summary>Class operations</summary>
+class Operations
+{
+    /// <summary>Adds two integers</summary>
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
