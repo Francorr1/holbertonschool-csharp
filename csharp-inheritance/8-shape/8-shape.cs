49acda11607981886d41afd6488e06232a92a511
Franco Correa <francorr240@gmail.com>
2024-01-20 10:55:47 -0300

    Task 8 complete

---
+++ csharp-inheritance/8-shape/8-shape.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+/// <summary>
+/// Class Shape
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Throws an exception
+    /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// Class Rectangle (from Shape)
+/// </summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+        }
+    }
+
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            height = value;
+        }
+    }
+
+    public override int Area()
+    {
+        return (Height * Width);
+    }
+
+    public override string ToString()
+    {
+        return ("[Rectangle] " + Width + " / " + Height);
+    }
+}
+
+/// <summary>
+/// Class Square (from Rectangle (from Shape))
+/// </summary>
+class Square : Rectangle
+{
+    private int size;
+    public int Size
+    {
+        get
+        {
+            return size;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            }
+            size = value;
+            Height = value;
+            Width = value;
+        }
+    }
+}
