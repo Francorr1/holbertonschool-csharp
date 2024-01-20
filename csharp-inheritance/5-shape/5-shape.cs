c984966e23a0209e1be71ffec8d3db33d9fb0e57
Franco Correa <francorr240@gmail.com>
2024-01-20 10:23:13 -0300

    Task 5 complete

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Class Shape
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Throws an exception
+    /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
