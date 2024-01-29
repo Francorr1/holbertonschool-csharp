51a3533c28b321cc211796c147e0e5acacf2ff85
Franco Correa <6203@hbtn.uy>
2024-01-29 12:33:44 -0300

    TAsk 3 complete

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Vectormath
+{
+    public static double Magnitude(double[] vector)
+    {
+        int res = 0;
+        int VectorType = vector.count();
+        if (VectorType == 2)
+        {
+            res += Math.Pow(vector[0], 2);
+            res += Math.Pow(vector[1], 2);
+            res = Math.Sqrt(res);
+            return res;
+        }
+        if (VectorType == 3)
+        {
+            res += Math.Pow(vector[0], 2);
+            res += Math.Pow(vector[1], 2);
+            res += Math.Pow(vector[2], 2);
+            res = Math.Sqrt(res);
+            return res;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
