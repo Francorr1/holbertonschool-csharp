9c01f324cddab74abd705014ab0e56cca9c4d980
Franco Correa <6203@hbtn.uy>
2024-01-29 13:11:47 -0300

    TAsk 6 complete

---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        int Vec1Type = vector1.Count();
+        int Vec2Type = vector2.Count();
+        if (Vec1Type != Vec2Type)
+        {
+            double[] res = [-1];
+            return res;
+        }
+        if (Vec1Type == 2)
+        {
+            double x = vector1[0] + vector2[0];
+            double y = vector1[1] + vector2[1];
+            double[] res = [x, y];
+            return res;
+        }
+        if (Vec1Type == 3)
+        {
+            double x = vector1[0] + vector2[0];
+            double y = vector1[1] + vector2[1];
+            double z = vector1[2] + vector2[2];
+            double[] res = [x, y, z];
+            return res;
+        }
+        else
+        {
+            double[] res = [-1];
+            return res;
+        }
+    }
+}
