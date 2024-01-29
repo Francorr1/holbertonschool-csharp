d5fe857d433d658c8aef492961f155b6666aa3c8
Franco Correa <6203@hbtn.uy>
2024-01-29 13:17:10 -0300

    Fixed a lot of shit

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -2,22 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 
+/// <summary>
+/// class VectorMath
+/// </summary>
 class VectorMath
 {
+    /// <summary>
+    /// Adds two 2D or 3D vectors
+    /// </summary>
+    /// <param name="vector1"></param>
+    /// <param name="vector2"></param>
+    /// <returns></returns>
     public static double[] Add(double[] vector1, double[] vector2)
     {
         int Vec1Type = vector1.Count();
         int Vec2Type = vector2.Count();
         if (Vec1Type != Vec2Type)
         {
-            double[] res = [-1];
+            double[] res = {-1};
             return res;
         }
         if (Vec1Type == 2)
         {
             double x = vector1[0] + vector2[0];
             double y = vector1[1] + vector2[1];
-            double[] res = [x, y];
+            double[] res = {x, y};
             return res;
         }
         if (Vec1Type == 3)
@@ -25,12 +34,12 @@ class VectorMath
             double x = vector1[0] + vector2[0];
             double y = vector1[1] + vector2[1];
             double z = vector1[2] + vector2[2];
-            double[] res = [x, y, z];
+            double[] res = {x, y, z};
             return res;
         }
         else
         {
-            double[] res = [-1];
+            double[] res = {-1};
             return res;
         }
     }
