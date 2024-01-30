6c9d13d1c6b6488994b939c6a7d80d1d4dd8fc9d
Franco Correa <6203@hbtn.uy>
2024-01-30 12:11:20 -0300

    Task 14 complete

---
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MatrixMath
+{
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        int Mat1Type = matrix1.GetLength(0);
+        int Mat2Type = matrix2.GetLength(0);
+        if (Mat1Type != Mat2Type)
+        {
+            double[] res = {-1};
+            return res;
+        }
+        if (Mat1Type == 2)
+        {
+            double[,] res = { { 0, 0 }, { 0, 0 } };
+            for (int i = 0; i < Mat1Type; i++)
+            {
+                for (int j = 0; j < Mat1Type; j++)
+                {
+                    res[i, j] += matrix1[i, j];
+                    res[i, j] += matrix2[i, j];
+                }
+            }
+            return res;
+        }
+        if (Mat1Type == 3)
+        {
+            double[,] res = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+            for (int i = 0; i < Mat1Type; i++)
+            {
+                for (int j = 0; j < Mat1Type; j++)
+                {
+                    res[i, j] += matrix1[i, j];
+                    res[i, j] += matrix2[i, j];
+                }
+            }
+            return res;
+        }
+        else
+        {
+            double[] res = {-1};
+            return res;
+        }
+    }
+}
