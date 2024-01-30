334504c985e04a8d6d75babb4ece8bc9849d312e
Franco Correa <6203@hbtn.uy>
2024-01-30 11:28:21 -0300

    Task 9 complete

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// class VectorMath
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Function that multiplies 2D or 3D vectors by a scalar
+    /// </summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        int VecType = vector.Count();
+        if (VecType == 2)
+        {
+            double[] res = {vector[0] * scalar, vector[1] * scalar};
+            return res;
+        }
+        if (VecType == 3)
+        {
+            double[] res = {vector[0] * scalar, vector[1] * scalar, vector[2] * scalar};
+            return res;
+        }
+        else
+        {
+            double[] res = {-1};
+            return res;
+        }
+    }
+}
