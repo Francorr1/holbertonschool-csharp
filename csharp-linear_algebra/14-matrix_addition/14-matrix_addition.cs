f11653d0e86719e3292e07599dc7f208aba2e2c8
Franco Correa <6203@hbtn.uy>
2024-01-30 12:41:07 -0300

    Fixed a corner case and added XML documentation

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 
+/// <summary>
+/// class MatrixMath
+/// </summary>
 class MatrixMath
 {
+    /// <summary>
+    /// Function that adds two 2D or 3D matrix
+    /// </summary>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        int Mat1Type = matrix1.Count();
-        int Mat2Type = matrix2.Count();
+        int Mat1Type = matrix1.GetLength(0) + matrix1.GetLength(1);
+        int Mat2Type = matrix2.GetLength(0) + matrix2.GetLength(1);
         if (Mat1Type != Mat2Type)
         {
             double[,] res = { { -1 } };
@@ -16,9 +22,9 @@ class MatrixMath
         if (Mat1Type == 4)
         {
             double[,] res = { { 0, 0 }, { 0, 0 } };
-            for (int i = 0; i < Mat1Type; i++)
+            for (int i = 0; i < 2; i++)
             {
-                for (int j = 0; j < Mat1Type; j++)
+                for (int j = 0; j < 2; j++)
                 {
                     res[i, j] += matrix1[i, j];
                     res[i, j] += matrix2[i, j];
@@ -29,9 +35,9 @@ class MatrixMath
         if (Mat1Type == 6)
         {
             double[,] res = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
-            for (int i = 0; i < Mat1Type; i++)
+            for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < Mat1Type; j++)
+                for (int j = 0; j < 3; j++)
                 {
                     res[i, j] += matrix1[i, j];
                     res[i, j] += matrix2[i, j];
