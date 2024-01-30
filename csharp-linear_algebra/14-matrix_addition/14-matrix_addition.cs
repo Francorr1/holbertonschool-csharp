69e13f61c8640894d6d9cb65c1f9710f39e9f732
Franco Correa <6203@hbtn.uy>
2024-01-30 12:30:37 -0300

    Fixed a corner case

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -6,14 +6,14 @@ class MatrixMath
 {
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        int Mat1Type = matrix1.GetLength(0);
-        int Mat2Type = matrix2.GetLength(0);
+        int Mat1Type = matrix1.Count();
+        int Mat2Type = matrix2.Count();
         if (Mat1Type != Mat2Type)
         {
             double[,] res = { { -1 } };
             return res;
         }
-        if (Mat1Type == 2)
+        if (Mat1Type == 4)
         {
             double[,] res = { { 0, 0 }, { 0, 0 } };
             for (int i = 0; i < Mat1Type; i++)
@@ -26,7 +26,7 @@ class MatrixMath
             }
             return res;
         }
-        if (Mat1Type == 3)
+        if (Mat1Type == 6)
         {
             double[,] res = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
             for (int i = 0; i < Mat1Type; i++)
