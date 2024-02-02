3c2b6482a5d0f094398d40da99f4703f05fba3e2
Franco Correa <6203@hbtn.uy>
2024-02-02 13:53:32 -0300

    Task 23 complete

--- csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 
+/// <summary>
+/// class MatrixMath
+/// </summary>
 class MatrixMath
 {
+    /// <summary>
+    /// Function that transposes a matrix
+    /// </summary>
     public static double[,] Transpose(double[,] matrix)
     {
-        if (matrix.GetLength(0))
+        if (matrix.GetLength(0) == 0)
         {
             double[,] errempt = new double[,] { { } };
             return errempt;
