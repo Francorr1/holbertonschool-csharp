9532989862c8d1ef1e08682c10fae17585e4824d
Franco Correa <6203@hbtn.uy>
2024-02-02 02:29:08 -0300

    Task 24 complete (finally)

--- csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 
+/// <summary>
+/// class MatrixMath
+/// </summary>
 class MatrixMath
 {
+    /// <summary>
+    /// Function that shears a matrix in a direction by a factor
+    /// </summary>
     public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
         if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
@@ -11,23 +17,21 @@ class MatrixMath
             double[,] errmat = { { -1 } };
             return errmat;
         }
-        if (direction != 'x' || direction != 'y')
-        {
-            double[,] errdir = { { -1 } };
-            return errdir;
-        }
         if (direction == 'x')
         {
-            double[,] res = { { matrix[1, 1], matrix[1, 2] }, { matrix[2, 1], matrix[2, 2] } };
-            res[1, 1] += factor * res[1, 2];
-            res[2, 1] += factor * res[2, 2];
+            double[,] res = { { matrix[0, 0], matrix[0, 1] }, { matrix[1, 0], matrix[1, 1] } };
+            res[0, 0] += factor * res[0, 1];
+            res[1, 0] += factor * res[1, 1];
+            return res;
         }
         if (direction == 'y')
         {
-            double[,] res = { { matrix[1, 1], matrix[1, 2] }, { matrix[2, 1], matrix[2, 2] } };
-            res[1, 2] += factor * res[1, 1];
-            res[2, 2] += factor * res[2, 1];
+            double[,] res = { { matrix[0, 0], matrix[0, 1] }, { matrix[1, 0], matrix[1, 1] } };
+            res[0, 1] += factor * res[0, 0];
+            res[1, 1] += factor * res[1, 0];
+            return res;
         }
-        return res;
+        double[,] errdir = { { -1 } };
+        return errdir;
     }
 }
