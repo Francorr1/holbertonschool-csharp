b3605e27825d493c203fef4fe0679df9f9c455c4
Franco Correa <6203@hbtn.uy>
2024-02-02 15:30:41 -0300

    trying a fix

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -12,9 +12,8 @@ class MatrixMath
     /// </summary>
     public static double Determinant(double[,] matrix)
     {
-        int MatType1 = matrix.GetLength(0);
-        int MatType2 = matrix.GetLength(1);
-        if ((MatType1 != 2 & MatType1 != 3) || (MatType2 != 2 && MatType2 != 3))
+        int MatType1 = (matrix.GetLength(0) + matrix.GetLength(1)) / 2;
+        if (MatType1 != 2 || MatType1 != 3)
         {
             return -1;
         }
@@ -29,6 +28,6 @@ class MatrixMath
             res -= matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] - matrix[1, 2] * matrix[2, 0]);
             res += matrix[0, 2] * (matrix[1, 0] * matrix[2, 1] - matrix[1, 1] * matrix[2, 0]);
         }
-        return res;
+        return Math.Round(res, 2);
     }
 }
