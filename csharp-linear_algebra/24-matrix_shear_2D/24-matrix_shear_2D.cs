d60d2efb342cd1d45173a05d07561a3c8149b0d0
Franco Correa <6203@hbtn.uy>
2024-02-02 02:18:08 -0300

    Task 24 complete

---
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MatrixMath
+{
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        {
+            double[,] errmat = { { -1 } };
+            return errmat;
+        }
+        if (direction != 'x' || direction != 'y')
+        {
+            double[,] errdir = { { -1 } };
+            return errdir;
+        }
+        if (direction == 'x')
+        {
+            double[,] res = { { matrix[1, 1], matrix[1, 2] }, { matrix[2, 1], matrix[2, 2] } };
+            res[1, 1] += factor * res[1, 2];
+            res[2, 1] += factor * res[2, 2];
+        }
+        if (direction == 'y')
+        {
+            double[,] res = { { matrix[1, 1], matrix[1, 2] }, { matrix[2, 1], matrix[2, 2] } };
+            res[1, 2] += factor * res[1, 1];
+            res[2, 2] += factor * res[2, 1];
+        }
+        return res
+    }
+}
