5d68594bed5d20fb338be61809d3afe10123be6b
Franco Correa <6203@hbtn.uy>
2024-02-03 13:59:34 -0300

    Task 31 complete

---
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MatrixMath
+{
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        {
+            double[,] errmat = { { -1 } };
+            return errmat;
+        }
+        double det = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+        if (det == 0)
+        {
+            double[,] errdet = { { -1 } };
+            return errdet;
+        }
+        double[,] res = { {matrix[1, 1], matrix[0, 1] * -1}, {matrix[1, 0] * -1, matrix[0, 0]} };
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                res[i, j] = res[i, j] / det;
+            }
+        }
+        return res;
+    }
+}
