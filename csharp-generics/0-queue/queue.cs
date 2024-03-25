54f6121738bd708df56702eab89b6718a2e759c0
Franco Correa <6203@hbtn.uy>
2024-03-25 12:30:14 -0300

    Task 0 complete

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Queue class
+/// </summary>
+/// <typeparam name="T">Representation of a queue</typeparam>
+class Queue<T>
+{
+    /// <summary>
+    /// Returns the type of the Queue T
+    /// </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
