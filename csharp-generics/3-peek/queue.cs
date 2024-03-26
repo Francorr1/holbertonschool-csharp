b394b527663cf761e24f784afaa6fb2ea0e9d792
Franco Correa <6203@hbtn.uy>
2024-03-26 10:23:13 -0300

    Task 3 complete

---
+++ csharp-generics/3-peek/queue.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+/// <summary>
+/// Queue class
+/// </summary>
+/// <typeparam name="T">Representation of a queue</typeparam>
+class Queue<T>
+{
+    public Node head { get; set; }
+    public Node tail { get; set; }
+    public int count;
+
+    /// <summary>
+    /// Returns the type of the Queue T
+    /// </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary>
+    /// Class Node
+    /// </summary>
+    public class Node
+    {
+        public T value { get; set; }
+        public Node next { get; set; }
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    /// <summary>
+    /// Method that creates a new Node and adds it to the queue
+    /// </summary>
+    public void Enqueue(T element)
+    {
+        Node node = new Node(element);
+        node.value = element;
+
+        if (head == null)
+        {
+            node.next = null;
+            head = node;
+            tail = node;
+        }
+        else
+        {
+            node.next = head;
+            head = node;
+        }
+        count += 1;
+    }
+
+    /// <summary>
+    /// Method that returns count
+    /// </summary>
+    /// <returns> count </returns>
+    public int Count()
+    {
+        return count;
+    }
+
+    /// <summary>
+    /// Gets rid of the first node of the queue
+    /// </summary>
+    /// <returns> The deleted node </returns>
+    public T Dequeue()
+    {
+        if (head == null && tail == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        Node node = head;
+        T fValue;
+        if (node.next == null)
+        {
+            fValue = node.value;
+            count -= 1;
+            head = null;
+            tail = null;
+        }
+        else
+        {
+            while (node.next != tail)
+            {
+                node = node.next;
+            }
+            fValue = tail.value;
+            count -= 1;
+            tail = node;
+            node.next = null;
+        }
+        return fValue;
+    }
+
+    public T Peek()
+    {
+        if (head == null && tail == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        else
+        {
+            return tail.value;
+        }
+    }
+}
