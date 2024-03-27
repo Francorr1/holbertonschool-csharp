f73e1b7b9089310f3cd099321c1daf4f8ab9b2f7
Franco Correa <6203@hbtn.uy>
2024-03-27 19:09:48 -0300

    Task 0 complete

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Base class
+/// </summary>
+abstract class Base
+{
+    public string name { get; set; }
+
+    /// <summary>
+    /// ToString() override
+    /// </summary>
+    /// <returns> New format </returns>
+    public override string ToString()
+    {
+        return ($"{this.name} is a {this.GetType()}");
+    }
+}
